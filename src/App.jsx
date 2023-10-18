19c471ef7b51b608596f55500aeb325d1806f9ce
Anirudha Havaldar <anirudhahavaldar@gmail.com>
2023-10-18 19:55:17 +0530

    03-jsx

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Pet from "./Pet";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" />
+    </div>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(React.createElement(App));
